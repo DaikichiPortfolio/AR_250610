e3cdd84b57c8261b7693d626a63ee7a223fb12d7
DaikichiPortfolio <113177552+DaikichiPortfolio@users.noreply.github.com>
2025-06-10 14:55:32 +0900

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2253344.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 232587289, 232587292, 232587264, 232587291, 232587293, 232587296, 232587295, 232587294, 232587299, 232587297, 232587298, 232587301, 232587302, 232587303 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/232587304/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/232587268/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/232587263/1/ammo.js', 'preload' : true},
+];
